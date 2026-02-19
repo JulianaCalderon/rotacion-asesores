5bde293f3e35810a19969334c62feb6ea50244d0
JulianaCalderon <147257963+JulianaCalderon@users.noreply.github.com>
2026-02-19 10:42:20 -0500

    Add files via upload

--- api/rotaje.js
+++ api/rotaje.js
@@ -1,15 +1,3 @@
-let current = 0;
-
-const numbers = [
-  "5466", 
-  "2197"
-];
-
 export default function handler(req, res) {
-  const number = numbers[current];
-  current = (current + 1) % numbers.length;
-
-  res.status(200).json({
-    redirect: `https://wa.me/${number}`
-  });
+  res.status(200).json({ message: "API está funcionando 🚀" });
 }
