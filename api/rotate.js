3b64c820a7866aae5ee1cb271a2baed73c4fa701
JulianaCalderon <147257963+JulianaCalderon@users.noreply.github.com>
2026-02-19 11:47:02 -0500

    Add files via upload

--- api/rotate.js
+++ api/rotate.js
@@ -1,5 +1,14 @@
+let turn = 0;
+
+const asesores = [
+  "https://wa.me/573142783363",
+  "https://wa.me/573228125466"
+  
+];
+
 export default function handler(req, res) {
-  res.status(200).json({
-    message: "API funcionando correctamente 🚀"
-  });
+  const url = asesores[turn];
+  turn = (turn + 1) % asesores.length;
+
+  res.status(200).json({ url });
 }
