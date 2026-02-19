6ff6f3b4ea8a12625083e080172cb11108611d4e
JulianaCalderon <147257963+JulianaCalderon@users.noreply.github.com>
2026-02-19 10:59:28 -0500

    Add files via upload

--- api/rotaje.js
+++ api/rotaje.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "API está funcionando 🚀" });
+  res.status(200).json({
+    message: "API funcionando correctamente 🚀"
+  });
 }
