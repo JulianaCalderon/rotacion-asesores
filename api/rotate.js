a0570c682a9df63a3e64e67caabadf176c9d14bc
JulianaCalderon <147257963+JulianaCalderon@users.noreply.github.com>
2026-02-20 15:08:32 -0500

    Add files via upload

--- api/rotate.js
+++ api/rotate.js
@@ -1,8 +1,8 @@
 let turn = 0;
 
 const asesores = [
-  "https://wa.me/573133262197",
-  "https://wa.me/573228125466"
+  "https://wa.me/573133262197?text=Hola%2C%20Mafe%2C%20deseo%20m%C3%A1s%20informaci%C3%B3n%2C%20por%20favor",
+  "https://wa.me/573228125466?text=Hola%2C%20Leidy%2C%20deseo%20m%C3%A1s%20informaci%C3%B3n%2C%20por%20favor"
   
 ];
 
